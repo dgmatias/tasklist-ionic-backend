9aa8e06ccd302c107e9f5b18f5c55cc31be15f7a
Diego Matias <dg.matias021@gmail.com>
2023-09-12 13:15:20 -0300

    Criando o getConnection - método que faz a ligação com o banco de dados

---
+++ src/main/java/dao/app.java
@@ -0,0 +1,11 @@
+package dao;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dao.getConnection();
+		System.out.println(Dao.getStatus());
+	}
+
+}
