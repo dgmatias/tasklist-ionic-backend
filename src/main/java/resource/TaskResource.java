171210e6b9080f80295177afb563bd19c0ad5127
Diego Matias <dg.matias021@gmail.com>
2023-09-15 15:36:54 -0300

    Criação do método setTask

--- src/main/java/resource/TaskResource.java
+++ src/main/java/resource/TaskResource.java
@@ -3,11 +3,13 @@ package resource;
 import java.util.List;
 
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.Response.Status;
 import model.Task;
 import service.Service;
 
@@ -30,8 +32,9 @@ public class TaskResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("listById/{id}")
-	public Response getTaskById(@PathParam("id") Integer id) {
+	@Path("listById")
+	//http://localhost:8080/tasklist/rest/task/listById?id=
+	public Response getTaskById(@QueryParam("id") Integer id) {
 		
 		Service service = new Service();
 		List<Task> list = service.searchByTaskId(id);
@@ -42,6 +45,33 @@ public class TaskResource {
 		
 	}
 	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("add")
+	public Response setTask(@QueryParam("name") String name) {
+		
+		Service service = new Service();
+		Task task = new Task();
+		
+		task.setName(name);
+		boolean result = service.addTask(task);
+		
+		Response response;
+		
+		if(result) {
+			
+			response = Response.status(Response.Status.CREATED).entity("{\"message\": \"Tarefa adicionada com sucesso\"}").build();
+			
+		} else {
+			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+	                .entity("{\"error\": \"Erro ao adicionar a tarefa\"}")
+	                .build();
+
+		}
+
+		return response;
+		
+	}
 	
 	
 }
