bbc7ac641fcd1c5a2567e4d914aeb36dfe701a53
Diego Matias <dg.matias021@gmail.com>
2023-09-17 23:07:01 -0300

    Concluíndo o método setTask

--- src/main/java/resource/TaskResource.java
+++ src/main/java/resource/TaskResource.java
@@ -2,9 +2,11 @@ package resource;
 
 import java.util.List;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
@@ -13,12 +15,13 @@ import jakarta.ws.rs.core.Response.Status;
 import model.Task;
 import service.Service;
 
-@Path("task")
+@Path("tasks")
 public class TaskResource {
 	
+	// http://localhost:8080/tasklist/rest/tasks/task
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("list")
+	@Path("task")
 	public Response getTasks() {
 		
 		Service service = new Service();
@@ -30,11 +33,11 @@ public class TaskResource {
 	}
 	
 	
+	// http://localhost:8080/tasklist/rest/tasks/task/id
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("listById")
-	//http://localhost:8080/tasklist/rest/task/listById?id=
-	public Response getTaskById(@QueryParam("id") Integer id) {
+	@Path("task/{id}")
+	public Response getTaskById(@PathParam("id") Integer id) {
 		
 		Service service = new Service();
 		List<Task> list = service.searchByTaskId(id);
@@ -45,26 +48,25 @@ public class TaskResource {
 		
 	}
 	
+	// http://localhost:8080/tasklist/rest/tasks/task/
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	@Path("add")
-	public Response setTask(@QueryParam("name") String name) {
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("task")
+	public Response setTask(Task task) {
 		
 		Service service = new Service();
-		Task task = new Task();
-		
-		task.setName(name);
 		boolean result = service.addTask(task);
-		
-		Response response;
+
+		Response response = null;
 		
 		if(result) {
 			
-			response = Response.status(Response.Status.CREATED).entity("{\"message\": \"Tarefa adicionada com sucesso\"}").build();
+			response = Response.status(Response.Status.CREATED).entity("Tarefa adicionada com sucesso").build();
 			
 		} else {
 			response = Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-	                .entity("{\"error\": \"Erro ao adicionar a tarefa\"}")
+	                .entity("Erro ao adicionar a tarefa")
 	                .build();
 
 		}
